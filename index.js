af5b6f7c1bdc7ff24bfc6204495a29df3b2c36af
jep4 <civbf1@gmail.com>
2023-07-19 23:51:50 -0700

    Initial server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'hello world' });
+})
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`server on port: ${port}`);
+})
